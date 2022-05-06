0665a5a46c60e02f6b3ca6a96e221c42d218ffbe
DyegoIthalo <dyegoithalo11@hotmail.com>
2022-05-06 00:43:08 -0300

    adicionando estilos, funções, acrescentando coisas no index.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ const aliensImg = [
   "img/monster-2.png",
   "img/monster-3.png",
 ];
+const instructionsText = document.querySelector(".game-instructions");
+const startButton = document.querySelector(".start-button");
+let alienInterval;
 
 //movimento e tiro da nave
 function flyShip(event) {
@@ -101,4 +104,69 @@ function createAliens() {
   moveAlien(newAlien);
 }
 
-window.addEventListener("keydown", flyShip);
+//função para movimentar os inimigos
+function moveAlien(alien) {
+  let moveAlienInterval = setInterval(() => {
+    let xPosition = parseInt(
+      window.getComputedStyle(alien).getPropertyValue("left")
+    );
+    if (xPosition <= 50) {
+      if (Array.from(alien.classList).includes("dead-alien")) {
+        alien.remove();
+      } else {
+        gameOver();
+      }
+    } else {
+      alien.style.left = `${xPosition - 4}px`;
+    }
+  }, 30);
+}
+
+//função para colisão
+function checkLaserCollision(laser, alien) {
+  let laserTop = parseInt(laser.style.top);
+  let laserLeft = parseInt(laser.style.left);
+  let laserBottom = laserTop - 20;
+  let alienTop = parseInt(alien.style.top);
+  let alienLeft = parseInt(alien.style.left);
+  let alienBottom = alienTop - 30;
+  if (laserLeft != 340 && laserLeft + 40 >= alienLeft) {
+    if (laserTop <= alienTop && laserTop >= alienBottom) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+//inicio do jogo
+startButton.addEventListener("click", (event) => {
+  playGame();
+});
+
+function playGame() {
+  startButton.style.display = "none";
+  instructionsText.style.display = "none";
+  window.addEventListener("keydown", flyShip);
+  alienInterval = setInterval(() => {
+    createAliens();
+  }, 2000);
+}
+
+//função de game over
+function gameOver() {
+  window.removeEventListener("keydown", flyShip);
+  clearInterval(alienInterval);
+  let aliens = document.querySelectorAll(".alien");
+  aliens.forEach((alien) => alien.remove());
+  let lasers = document.querySelectorAll(".laser");
+  lasers.forEach((laser) => laser.remove());
+  setTimeout(() => {
+    alert("game over!");
+    yourShip.style.top = "250px";
+    startButton.style.display = "block";
+    instructionsText.style.display = "block";
+  });
+}
